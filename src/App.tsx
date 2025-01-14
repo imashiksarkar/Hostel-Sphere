a5f478fabd28fb8f4ae28ce96d0c4a60426b9088
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2025-01-14 22:55:00 +0600

    - init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './assets/css/Global.css'
+import logo from './assets/images/logo.png'
+
+const App = () => {
+  return (
+    <div>
+      <img src={logo} alt='logo' />
+    </div>
+  )
+}
+
+export default App
