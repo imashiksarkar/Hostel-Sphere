32a4c2fdcc7c3dbb1c9d9e92224b2dd5bda6c3ee
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2025-01-18 07:17:43 +0600

    - fix. layout navbar color

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,18 @@
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
-import { Outlet } from 'react-router'
+import { cn } from '@/lib/utils'
+import { Outlet, useLocation } from 'react-router'
 
 const MainLayout = () => {
+  const { pathname } = useLocation()
   return (
     <div className='layout main-layout min-h-dvh w-full grid grid-flow-row grid-cols-1 grid-rows-[auto_1fr_auto] [&>*]:col-start-1'>
-      <Navbar className='row-start-1 z-10 bg-transparent' />
+      <Navbar
+        className={cn(
+          'row-start-1 z-10',
+          pathname === '/' && ' bg-transparent'
+        )}
+      />
       <Outlet />
       <Footer />
     </div>
